6e9cb0d260347ddc7d94e5403ef93fdb434e0dfa
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-05 17:39:05 +0300

    improve basic view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,20 @@
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
+            <th>Id</th>
             <th>Name</th>
+            <th>Position</th>
         </tr>
         </thead>
         <tbody>
             @foreach($employees as $employee)
                 <tr>
+                    <td>{{ $employee->id }}</td>
                     <td>
                         @for ($i = 0; $i < $employee->depth; $i++) &mdash; @endfor
                         <a href="#">{{ $employee->full_name }}</a>
                     </td>
+                    <td>{{ $employee->title->name }}</td>
                 </tr>
             @endforeach
         </tbody>
