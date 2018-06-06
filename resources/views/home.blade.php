cc48ee7e5d03562cf4780dfa6074ff23a7aa6619
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-06 13:20:04 +0300

    fix formating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h4>Boss (as position title) is not an employee, but parent for each node. So Boss isn't shown in the tree, but each employee has a chief.</h4>
     @if(!empty($boss))
-        <p>In current migrations Boss is {{ $boss->full_name }} - ( {{ $boss->title->name }} )</p>
+        <p>In current migrations Boss is <strong>{{ $boss->full_name }}</strong> - ( title: {{ $boss->title->name }} )</p>
     @endif
     <hr>
     <div id="tree"></div>
