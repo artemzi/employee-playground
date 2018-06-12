63047ca1a22b7df1b07067272472887a057b6398
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-12 17:25:08 +0300

    change nginx upload limit

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,14 +1,15 @@
 require('./bootstrap');
 
 $(function() {
-    $('#delete__action').mouseenter(function() {
-        $( "#delete__action" )
+    action = $('#delete__action');
+    action.mouseenter(function() {
+        action
             .parent()
             .append( "<div class='row' id='message__warning'><div class='col-12 alert alert-warning text-center'>" +
                 "Please reassign child's before remove parent node. Node will be removed completely with ALL current descendants." +
                 "</div></div>" );
     });
-    $('#delete__action').mouseleave(function() {
+    action.mouseleave(function() {
         $( "#message__warning" ).remove();
     });
 
