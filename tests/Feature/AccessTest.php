88cbea3305f22d00e1241cf3794d7aeeebb76e14
Artem Zinoviev <arzinoviev@gmail.com>
2018-06-13 16:13:49 +0300

    add first test

---
+++ tests/Feature/AccessTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use EmployeeDirectory\Entity\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class AccessTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function guestCanBrowseMainPage(): void
+    {
+        $this->get('/')
+            ->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function guestCanNotBrowseDetailsPage(): void
+    {
+        $this->get('/admin/employees')
+            ->assertRedirect('/login');
+    }
+
+    /**
+     * @test
+     */
+    public function userCanBrowseDetailsPage(): void
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get('/admin/employees');
+        $response->assertStatus(200);
+    }
+}
